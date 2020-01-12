77d38300c79b7966dc0d07c2b22d8e21785514ca
tbportwood <tbportwood@gmail.com>
2020-01-11 20:32:05 -0500

    .

---
+++ VacationPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,3 @@
+g_key = 'AIzaSyCGeIKyGzL60oYV0HZO7W_na5ouTnxCiC4'
+
+weather_api_key = 'efee55ba9584729e369bf910dee7a12e'
---
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,3 @@
+g_key = 'AIzaSyCGeIKyGzL60oYV0HZO7W_na5ouTnxCiC4'
+
+weather_api_key = '8f9e4d619ccb7e42b051adaa74124297'
